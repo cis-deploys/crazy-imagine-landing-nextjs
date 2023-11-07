3438617bf29dae191d80383699492f3ed7deb2b2
Eduardo_cas123 <juegoseduardo09@gmail.com>
2023-11-07 14:17:46 -0400

    add listing Projects section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,20 +17,80 @@ const ContactSection = dynamic(
   { ssr: false },
 )
 
+const ProjectsTable = dynamic(
+  () => import("../components/ProjectsTable"),
+  { ssr: false },
+)
+
+export async function getServerSideProps() {
+  const domain = process.env.NEXT_PUBLIC_CRAZY_STRAPI_URL
+  const resArticles = await fetch(`${domain}projects?populate=*`)
+  const projects = await resArticles.json()
+
+  return { props: { projects } }
+}
+
 function Projects({ projects }) {
-    const { t } = useTranslation()
-    console.log(projects)
+  const { t } = useTranslation()
+  // console.log(projects.data)
+  
+  const projectsNew = [];
+
+  projects?.data.map(({ 
+    attributes:{
+      Key,
+      createdAt,
+      description,
+      details,
+      galleryImages,
+      images,
+      locale,
+      localizations,
+      moreAbout,
+      publishedAt,
+      seo,
+      slug,
+      title,
+      updatedAt,
+    }} ) => {
+      
+    const imagesArticles = [];
+    if(images){
+      images.data.map(({ attributes: { url }}) => {
+        imagesArticles.push({
+          url,//: `${domain}${url}`
+        });
+      });
+    }
+    projectsNew.push({
+      title,
+      description,
+      details,
+      moreAbout,
+      slug,
+      Key,
+      createdAt,
+      locale,
+      images: imagesArticles,
+      galleryImages,
+      seo
+    });
+    
+  });
 
   return (
     <Layout>
       <Box overflow="hidden">
-        <SectionHeader
-          title={t("services_sectionHeader_title")}
+        <SectionHeader 
+          title={t("common_button_projects")}
           img={headerImage}
           btn={true}
           cls="textContainer"
         />
-        <ContactSection />
+        <ProjectsTable
+          AllArticles={projectsNew}
+        />
+        <ContactSection/>
       </Box>
     </Layout>
   )
