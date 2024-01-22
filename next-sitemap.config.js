0142d29897777bedccd1e2d844fdf3b25d59671f
Maria Rodriguez <marialerodrio@gmail.com>
2024-01-22 15:29:44 -0400

    next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    siteUrl: 'https://www.crazyimagine.com',
+    generateRobotsTxt: true,
+    // alternateRefs: [
+    //   {
+    //     href: 'https://www.crazyimagine.com/',
+    //     hreflang: 'en',
+    //   },
+    //   {
+    //     href: 'https://www.crazyimagine.com/es',
+    //     hreflang: 'es',
+    //   },
+    // ],
+    // ...other options
+  }
