02a519ed139fb2dec600f4ee87cc4c79124440eb
mariacabriles29 <mecp2992@gmail.com>
2024-02-28 16:50:53 -0400

    feat: adding changes to the meet the team page design

--- src/pages/meet-team.js
+++ src/pages/meet-team.js
@@ -1,94 +1,114 @@
 import React, { useEffect, useState } from "react"
-import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import dynamic from 'next/dynamic'
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import dynamic from "next/dynamic"
 
 import Layout from "../components/Layout"
 
-import headerImage from "../../public/marciano.webp"
+import headerImage from "../../public/layer2.svg"
 import { NextSeo } from "next-seo"
 import { useRouter } from "next/router"
 
-const SectionHeader = dynamic(
-  () => import("../components/SectionHeader"),
-  { ssr: false },
-)
+const SectionHeader = dynamic(() => import("../components/SectionHeader"), {
+  ssr: false,
+})
 
-const MeetTeamSection = dynamic(
-    () => import("../components/MeetTeamSection"),
-    { ssr: false },
-  )
+const MeetTeamSection = dynamic(() => import("../components/MeetTeamSection"), {
+  ssr: false,
+})
 
-const ContactSection = dynamic(
-  () => import("../components/ContactSection"),
-  { ssr: false },
-)
+const ContactSection = dynamic(() => import("../components/ContactSection"), {
+  ssr: false,
+})
 
 export async function getServerSideProps({ locale }) {
   const domain = process.env.NEXT_PUBLIC_CRAZY_STRAPI_URL
 
-  const resArticles = await fetch(`${domain}members?members?populate=name&populate=role&populate=avatar`)
+  const resArticles = await fetch(
+    `${domain}members?members?populate=name&populate=role&populate=avatar`
+  )
   const members = await resArticles.json()
 
-  const resMeetTeam = await fetch(`${domain}meet-team-page?locale=${locale}&populate=title&populate=seo`)
-  const meetTeamPage= await resMeetTeam.json()
+  const resMeetTeam = await fetch(
+    `${domain}meet-team-page?locale=${locale}&populate=title&populate=seo`
+  )
+  const meetTeamPage = await resMeetTeam.json()
 
-  return { props: { members, meetTeamPage,
-    ...await serverSideTranslations(locale, ['common']),
-  } }
+  return {
+    props: {
+      members,
+      meetTeamPage,
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  }
 }
 
 const About = ({ members, meetTeamPage }) => {
-  const { t, i18n } = useTranslation('common')
+  const { t, i18n } = useTranslation("common")
   const router = useRouter()
 
   useEffect(() => {
     // Obtener la locale del router
-    const locale = router.locale;
+    const locale = router.locale
 
-    if (locale === 'es' && i18n.language !== 'es') {
+    if (locale === "es" && i18n.language !== "es") {
       // Establecer el idioma en español si no está establecido
-      i18n.changeLanguage('es');
+      i18n.changeLanguage("es")
     }
-  }, [router.locale, i18n]);
+  }, [router.locale, i18n])
 
-  const [metaTitle, setMetaTitle] = useState();
-  const [metaDescription, setMetaDescription] = useState();
-  const [keywords, setKeywords] = useState();
-  const [ title, setMainTitle ] = useState(); 
+  // const button = {
+  //   refID: "contactSection",
+  //   text: t("common_button_SCHEDULE_A_CALL"),
+  // }
+  const [metaTitle, setMetaTitle] = useState()
+  const [metaDescription, setMetaDescription] = useState()
+  const [keywords, setKeywords] = useState()
+  const [title, setMainTitle] = useState()
 
-  const membersNew = [];
+  const membersNew = []
 
-  members.data.map(({ id, attributes: { name, role, avatar}}) => {
-    const imagesAvatar = [];
-    if(avatar.data){
-      avatar.data.map(({ attributes: { url }}) => {
-        imagesAvatar.push({
-          url,//: `${domain}${url}`
-        });
-      });
+  members.data.map(
+    ({ id, attributes: { name, role, avatar, cardDescription } }) => {
+      const imagesAvatar = []
+      if (avatar.data) {
+        avatar.data.map(({ attributes: { url } }) => {
+          imagesAvatar.push({
+            url, //: `${domain}${url}`
+          })
+        })
+      }
+      membersNew.push({
+        id,
+        name,
+        role,
+        avatar: imagesAvatar,
+        cardDescription,
+      })
     }
-    membersNew.push({
-      id,
-      name,
-      role,
-      avatar: imagesAvatar,
-    });
-  });
+  )
 
   useEffect(() => {
     setMetaTitle(meetTeamPage.data?.attributes.seo?.metaTitle),
-    setMetaDescription(meetTeamPage.data?.attributes.seo?.metaDescription),
-    setKeywords(meetTeamPage.data?.attributes.seo?.keywords)
+      setMetaDescription(meetTeamPage.data?.attributes.seo?.metaDescription),
+      setKeywords(meetTeamPage.data?.attributes.seo?.keywords)
     setMainTitle(meetTeamPage.data?.attributes.title)
   }, [meetTeamPage])
 
   return (
-    <Layout >
+    <Layout>
       <NextSeo
         title={`Crazy Imagine Software | ${metaTitle ? metaTitle : title}`}
-        description={`${metaDescription ? metaDescription : 'Crazy Imagine Software Offer Software Development of High-Quality Web and Mobile Applications To Meet Our Client’s Unique Demands. Contac Us!'}`}
-        keywords={`${keywords ? keywords : 'crazy imagine, web development services, mobile app development, Software Development Company, Web and Mobile App Development Firm, developer, software, work, Full-stack Development, programming, user Experience, quality support'}`}
+        description={`${
+          metaDescription
+            ? metaDescription
+            : "Crazy Imagine Software Offer Software Development of High-Quality Web and Mobile Applications To Meet Our Client’s Unique Demands. Contac Us!"
+        }`}
+        keywords={`${
+          keywords
+            ? keywords
+            : "crazy imagine, web development services, mobile app development, Software Development Company, Web and Mobile App Development Firm, developer, software, work, Full-stack Development, programming, user Experience, quality support"
+        }`}
         openGraph={{
           type: "website",
           locale: "en_US",
@@ -97,12 +117,13 @@ const About = ({ members, meetTeamPage }) => {
       />
       <SectionHeader
         title={t("meetTheTeam_sectionHeader_title")}
-        btn={false}
+        desc={t("meetTheTeam_sectionHeader_desc")}
+        btn={true}
         img={headerImage}
         cls="textContainer"
       />
-      <MeetTeamSection members={membersNew}/>
-      <ContactSection />
+      <MeetTeamSection members={membersNew} />
+      {/* <ContactSection /> */}
     </Layout>
   )
 }
